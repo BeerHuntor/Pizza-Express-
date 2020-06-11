83f70317201601af79751ab2f55cc834e2ef2216
HuntorGames <rt001a9138@gmail.com>
2020-06-11 23:15:15 +0100

    Added sprite script to controll the movement of the happy customer sprite and deletion

---
+++ Assets/Scripts/SpriteScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScript : MonoBehaviour
+{
+    private float speed = 3f;
+    private float movementBeforeDestroy = 3f;
+    private float positionZ;
+
+    private void Start()
+    {
+        positionZ = transform.position.z;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < positionZ + movementBeforeDestroy)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
