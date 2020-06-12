8e4b0419e6cd7c26a4d200c7e06b83bd7049e45d
HuntorGames <rt001a9138@gmail.com>
2020-06-12 19:33:48 +0100

    Sound calls for buttons

---
+++ Assets/Scripts/Utiility/UIButtonSoundEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButtonSoundEvent : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+    //Called when the mouse button is pressed on the button.
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        AudioManager.instance.PlaySound(AudioManager.SoundType.MENU_CLICK);
+    }
+
+    //Called when the mouse cursor enters the button.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AudioManager.instance.PlaySound(AudioManager.SoundType.MENU_HOVER);
+    }
+
+}
